36790add10689b5a01f78b8ec2c86d8063f95a3d
ArbalesterCZ <ArbalesterCZ@gmail.com>
2026-04-13 10:19:32 +0200

    Project initialization.

---
+++ contact.js
@@ -0,0 +1,12 @@
+const contact_name = document.getElementById('name');
+const contact_service = document.getElementById('service');
+const contact_message = document.getElementById('message');
+
+
+function openMailClient() {
+    if (!document.getElementById('form').checkValidity())
+        return
+
+    const subject = contact_name.value + ": " + contact_service.value;
+    window.location.href = `mailto:RRsadrokartony@seznam.cz?subject=${subject}&body=${contact_message.value}`;
+}
